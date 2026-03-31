fe92a56409579de514c8f323aa0c689f9345c104
DevManTowhid <mdtowhidulalam364@gmail.com>
2026-03-31 15:43:35 +0600

    added meal details get api

--- backend/api/get.py
+++ backend/api/get.py
@@ -1,5 +1,11 @@
+from datetime import date
+
 from fastapi import APIRouter
 router = APIRouter()
+
+from models import student
+
+
 
 @router.get("/", tags=["status"])
 async def check_health():
@@ -11,6 +17,16 @@ async def check_health():
 @router.get("/users/{role}/{user_id}", tags=["status"])
 async def get_user(user_id: int, role: str):
     return {"user_id": user_id, "role": role}
+
+
+
+router.get("/meals/{date}/{hall_id}/{meal}", tags=["status"])
+async def get_meal(date: date,hall_id: int, meal: str):
+    return {"hall_id": hall_id, "meal": meal, "total_meal_numbers":[], "recipients":[]}
+
+
+
+
 
 
 
